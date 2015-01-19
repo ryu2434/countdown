45f14c2e66bcd49ffdce2036469711a7c89230ff
code-ninja{don} <stringhamdb@gmail.com>
2015-01-19 10:41:14 -0700

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,20 @@
-//change the text below to reflect your own,
-var before = "Groundhog's Day!";
-var current = "Today is Groundhog's Day!";
-var montharray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+// Function to complete.
 
-function countdown(yr, m, d) {
- // ...
+function CountDownTimer(dateTime, divId) {
+    var end     = new Date(dateTime);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour   = _minute * 60;
+    var _day    = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+       // ... 
+    }
+
+    timer = setInterval(showRemaining(), 1000);
 }
 
-//enter the count down date using the format year/month/day
-countdown(2015, 2, 2);
+// Enter the date and time as a string in the following format.  Second parameter is the HTML id of element where you want
+//    the countdown to appear.
+CountDownTimer('02/02/2015 12:00 AM', 'countDown');
