aeb2f34fa8ca7c7471f2f3e4332f7b75b58edaf1
Chase Stewart <ryu2434@hotmail.com>
2015-01-26 21:56:21 -0700

    First attempt at countdown assignment

--- js/countdown.js
+++ js/countdown.js
@@ -8,12 +8,27 @@ function CountDownTimer(dateTime, divId) {
     var timer;
 
     function showRemaining() {
-       // ... 
+        var now = new Date();
+        var distance = end - now;
+
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+        
+        //document.getElementById(divId).innerHTML = "GROUNDHOGS DAY WILL BE IN: "
+        document.getElementById(divId).innerHTML = 'GOUNDHOG\'S DAY WILL BE IN: '
+             + days + ' DAYS ' + hours + ' HOURS ' + minutes + ' MINUTES ' + seconds + ' SECONDS ';
+        location.reload();
+
     }
 
     timer = setInterval(showRemaining(), 1000);
+
+
 }
 
 // Enter the date and time as a string in the following format.  Second parameter is the HTML id of element where you want
 //    the countdown to appear.
+//countDown(9);
 CountDownTimer('02/02/2015 12:00 AM', 'countDown');
